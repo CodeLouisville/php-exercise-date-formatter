e3908f2ad8d8150f447e75b2139739321c35f64a
Korey McMurtrey <kmcmurtrey@gmail.com>
2016-01-21 19:53:54 -0500

    Revised code in class

--- src/date_formatter.php
+++ src/date_formatter.php
@@ -2,13 +2,44 @@
 
 /*|| $newDate->getLastErrors()['warning_count'] > 0*/
 
+//function date_formatter($date) {
+//    if (strtotime($date) === false) {
+//        return '';
+//    }
+//    
+//    $newDate = new DateTime($date);  
+//  
+//  if ($newDate->createFromFormat('Y-m-d', $date) === false || $newDate->getLastErrors()['warning_count'] > 0) {
+//    return '';
+//  } else {
+//    return $newDate->format('l\, F jS Y');
+//  }
+//}
+
+
+
 function date_formatter($date) {
-//  $validDate = checkdate ($date);
-  $newDate = new DateTime($date);  
+    $array = explode('-', $date);
+    
+    if (count($array) === 3) {
+    $validDate = checkdate ((int)$array[1], (int)$array[2], (int)$array[0]);
+    } else {
+        return '';
+    }
+        
+    if (strtotime($date) === false) {
+        return 'str';
+    }
+    
+    if ($validDate) {
+        $newDate = new DateTime($date);  
   
-  if ($newDate->createFromFormat('Y-m-d', $date) === false || $newDate->getLastErrors()['warning_count'] > 0) {
-    return '';
-  } else {
-    return $newDate->format('l\, F jS Y');
-  }
+        if ($newDate->createFromFormat('Y-m-d', $date) === false || $newDate->getLastErrors()['warning_count'] > 0) {
+            return '';
+        } else {
+            return $newDate->format('l\, F jS Y');
+        }
+    }
+    
+    return 'no';
 }
