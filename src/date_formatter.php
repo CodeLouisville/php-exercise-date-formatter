1a40007f5d60b43a42a78cd6aa33815286f6f17f
kmcmurtrey <korey.mcmurtrey@gmail.com>
2016-01-26 22:23:48 -0500

    All tests pass except 1776 test data fails with strtotime()

--- src/date_formatter.php
+++ src/date_formatter.php
@@ -6,9 +6,9 @@
 //    if (strtotime($date) === false) {
 //        return '';
 //    }
-//    
-//    $newDate = new DateTime($date);  
-//  
+//
+//    $newDate = new DateTime($date);
+//
 //  if ($newDate->createFromFormat('Y-m-d', $date) === false || $newDate->getLastErrors()['warning_count'] > 0) {
 //    return '';
 //  } else {
@@ -20,26 +20,28 @@
 
 function date_formatter($date) {
     $array = explode('-', $date);
-    
+
     if (count($array) === 3) {
     $validDate = checkdate ((int)$array[1], (int)$array[2], (int)$array[0]);
+
+
     } else {
         return '';
     }
-        
-    if (strtotime($date) === false) {
-        return 'str';
+
+    if (strtotime($date) === false || !$validDate) {
+      return '';
     }
-    
+
     if ($validDate) {
-        $newDate = new DateTime($date);  
-  
+        $newDate = new DateTime($date);
+
         if ($newDate->createFromFormat('Y-m-d', $date) === false || $newDate->getLastErrors()['warning_count'] > 0) {
             return '';
         } else {
             return $newDate->format('l\, F jS Y');
         }
     }
-    
-    return 'no';
+
+    return '';
 }
