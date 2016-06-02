feb7026e49e882ddfef7091e395b72fb4a73eed7
honggaojob@gmail.com <honggaojob@gmail.com>
2016-06-02 19:40:33 -0400

    explode and checkdate added

--- src/date_formatter.php
+++ src/date_formatter.php
@@ -1,17 +1,21 @@
 <?php
 
 //$date = "2016-01-14";
-$date = '2000-31-12';
+$date = '1-3-2016';
 
+$myDate = explode('-', $date);
 
 function date_formatter ($date){
-	try {
-		return (new DateTime($date))->format("l, F jS Y");
-	} catch (Exception $e){
+	$myDate = explode('-', $date);
+	if (checkdate($myDate[1], $myDate[2], $myDate[0]) === false){
 		return "";
+	}
+	else {
+		return (new DateTime($date))->format("l, F jS Y");
 	}
 }
 
 var_dump (date_formatter($date));
 
 ?>
+
