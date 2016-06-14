e377c0d7d3ca6904e3848fd88913aec435eb87a6
honggaojob@gmail.com <honggaojob@gmail.com>
2016-06-14 10:19:43 -0400

    check the format of date June 14

--- src/date_formatter.php
+++ src/date_formatter.php
@@ -1,21 +1,23 @@
 <?php
 
-$date = "2012-01-14";
-
 function date_formatter ($date){
-	$myDate = explode('-', $date);
-	if(strlen($myDate[0]) != 4 || strlen($myDate[1]) != 2 || strlen($myDate[2]) != 2){
+	if(gettype($date) !== 'string'){
 		return "";
-		}
-	elseif (checkdate($myDate[1], $myDate[2], $myDate[0]) === false){
+	}
+	$date_parse = explode('-', $date);
+	if(count($date_parse) !== 3){
+		return "";
+	}
+	$year = $date_parse[0];
+	$month = $date_parse[1];
+	$day = $date_parse[2];
+	if (checkdate($year, $month, $day) === false){
 		return "";
 	}
 	else {
 		return (new DateTime($date))->format("l, F jS Y");
 	}
 }
-
-//var_dump (date_formatter($date)); 
 
 ?>
 
