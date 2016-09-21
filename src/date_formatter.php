0ea49f6f62292dc0cdf5c4747a4bae84f058504b
jnels <jnels@users.noreply.github.com>
2016-09-21 10:48:02 -0400

    Having a go at it

---
+++ src/date_formatter.php
@@ -0,0 +1,20 @@
+<?php
+
+function format_date(string $str) {
+    if (strlen($str) !== 10) {
+        return "";
+    }
+    
+    $date = explode("-", $str);
+    
+    if (count($date) !== 3) {
+        return "";
+    }
+    
+    if (checkdate($date[1], $date[2], $date[0])) {
+        $date = new DateTime($str);
+        return $date->format('l, jS F Y');
+    } else {
+        return "";
+    }
+}
