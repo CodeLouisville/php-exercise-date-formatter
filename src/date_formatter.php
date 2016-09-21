31dc55333d20a48aea68b0b4efa73c12c3f9bfdf
jnels <jnels@users.noreply.github.com>
2016-09-21 11:50:18 -0400

    Fitter, happier, more productive

--- src/date_formatter.php
+++ src/date_formatter.php
@@ -1,5 +1,4 @@
 <?php
-
 function date_formatter($str) {
     
     $date = explode("-", $str);
@@ -8,10 +7,18 @@ function date_formatter($str) {
         return "";
     }
     
+    foreach ($date as $num) {
+        if (!is_numeric($num)) {
+            echo "not number ";
+            return "";
+        }
+    }
+    
     if (checkdate($date[1], $date[2], $date[0])) {
         $date = new DateTime($str);
         return $date->format('l, F jS Y');
     } else {
+        echo "fail ";
         return "";
     }
 }
