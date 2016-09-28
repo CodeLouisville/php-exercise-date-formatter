df78a2149e2f104880c95be483d22aabce151410
Kathryn <kanne1126@gmail.com>
2016-09-27 21:39:10 -0400

    month formatted

--- src/date_formatter.php
+++ src/date_formatter.php
@@ -1,6 +1,9 @@
 <?php
 function validateDate($date){
-  if(preg_match('/\d{4}-\d{2}-\d{2}/',$date)){
+  if(preg_match('/\d{4}-\d{2}-\d{2}/',$date) ||
+    preg_match('/\d{4}-\d{1}-\d{2}/',$date) ||
+    preg_match('/\d{4}-\d{1}-\d{1}/',$date) ||
+    preg_match('/\d{4}-\d{2}-\d{1}/',$date)  ) {
     return true;
   }else{
     return false;
@@ -9,9 +12,9 @@ function validateDate($date){
 function date_formatter($date){
   if (is_string($date) && validateDate($date)){
     $date = date_create($date);
-    return date_format($date, 'l, FjS Y');
+    return date_format($date, 'l, F jS Y');
   }else {
-     echo "";
+     return "";
   }
 }
 ?>
