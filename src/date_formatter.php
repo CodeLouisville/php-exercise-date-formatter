b7a26fa75aa23df63d4e6acdd1fe5d5cb70a3563
Kathryn <kanne1126@gmail.com>
2016-09-27 21:17:21 -0400

    date formatter

--- src/date_formatter.php
+++ src/date_formatter.php
@@ -1,28 +1,17 @@
 <?php
-
-function validateDate($date, $format = 'Y-m-d')
-{
-    $d = DateTime::createFromFormat($format, $date);
-    return $d && $d->format($format) == $date;
-}
-
-
-
-function date_formatter($date){
-
-  if (is_string($date)){
-    $date = date_create($date);
-
-    return date_format($date, 'l, FjS Y');
-
-  }else {
-     echo "No"; //change to return "";
+function validateDate($date){
+  if(preg_match('/\d{4}-\d{2}-\d{2}/',$date)){
+    return true;
+  }else{
+    return false;
   }
 }
-
-echo date_formatter("2016-01-16");
-
-
-
-
+function date_formatter($date){
+  if (is_string($date) && validateDate($date)){
+    $date = date_create($date);
+    return date_format($date, 'l, FjS Y');
+  }else {
+     echo "";
+  }
+}
 ?>
